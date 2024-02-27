fff7db37b133bfa2151c9e0c84c420ab14df9a2f
akaunderr <akaunderr@gmail.com>
2024-02-27 16:13:53 +0200

    uploaded aux. files

---
+++ ocmodw.ini.php
@@ -0,0 +1,29 @@
+<?php
+
+define('MODVERS', '3.8.1');
+define('MODYEAR', '2019');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK0', 'https://www.ocmod.space/live-product');
+define('MODLINK1', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=35460');
+
+define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=36005');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-live-product');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+
+define('LICLINK0', 'https://raw.githubusercontent.com/ocmod-space/ocmod-live-product/main/EULA.txt');
+define('LICLINK1', 'https://raw.githubusercontent.com/ocmod-space/ocmod-live-product/main/addons/ultimate/EULA.txt');
+
+///
+
+define('FCLDIR', '_fcl');
+define('FCLIGNORE', '.fclignore'); // ignorelist for fcl
+define('SRCDIR', 'src');
+define('ZIPDIR', 'zip');
+define('ZIPEXT', '.ocmod.zip');
+
+define('MDIR', 'module'); // dir with module
+define('ADIR', 'addons'); // dir with addons
+
